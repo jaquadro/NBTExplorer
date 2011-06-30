6109758896b534bb43348dfc85442d4e2d08abbb
Justin Aquadro <jaquadro@gmail.com>
2011-06-30 04:45:40 +0000

    Also missing files

---
+++ Substrate/SubstrateCS/Source/Orientation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Substrate
+{
+    public class Orientation
+    {
+        public double Pitch { get; set; }
+        public double Yaw { get; set; }
+    }
+}
---
+++ Substrate/SubstrateCS/Source/Vector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Substrate
+{
+    public class Vector3
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+}
