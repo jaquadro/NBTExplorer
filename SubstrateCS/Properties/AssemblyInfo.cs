432d87ac4f4f72f3e7799f5f92cee37067d805eb
Justin Aquadro <jaquadro@gmail.com>
2011-11-09 22:12:12 -0500

    Assembly version

--- SubstrateCS/Properties/AssemblyInfo.cs
+++ SubstrateCS/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.4.0")]
-[assembly: AssemblyFileVersion("0.8.4.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 
 // This library is compatible with all CLS-compliant .NET programming languages.
 [assembly: CLSCompliant(true)]
