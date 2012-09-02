af2702bb35d697dbce8096c0ebe76ad00ab02e4d
Justin Aquadro <jaquadro@gmail.com>
2012-09-02 00:58:53 -0400

    CLeanup and versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NBTExplorer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Graphical editor for most NBT data sources.  NBT is used by Minecraft.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NBTExplorer")]
-[assembly: AssemblyCopyright("Copyright © Justin Aquadro 2011")]
+[assembly: AssemblyCopyright("Copyright © Justin Aquadro 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
