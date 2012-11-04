436ac5e97f3e94833355a3f28ab4d02e3e63ee59
Justin Aquadro <jaquadro@gmail.com>
2012-11-04 01:45:51 -0400

    Icons in tree view partially working

---
+++ NBTExplorerMac/Mac/IconRegistry.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NBTExplorer.Mac
+{
+	public class IconRegistry
+	{
+		public IconRegistry ()
+		{
+		}
+	}
+}
+
---
+++ NBTExplorerMac/Mac/ImageAndTextCell.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NBTExplorer.Mac
+{
+	public class ImageAndTextCell
+	{
+		public ImageAndTextCell ()
+		{
+		}
+	}
+}
+
